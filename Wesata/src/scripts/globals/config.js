a80d474007a821322002773cfef52afa815ec49b
Friska Putri Rahma S <75847086+friskapr@users.noreply.github.com>
2022-06-13 10:19:14 +0700

    Fix Commit Responsive Home

--- Wesata/src/scripts/globals/config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-    BASE_URL: 'https://628d6555a339dfef879a3060.mockapi.io/',
-    CACHE_NAME: 'WeSata-V1',
-    DATABASE_NAME: 'wesata-database',
-    DATABASE_VERSION: 1,
-    OBJECT_STORE_NAME: 'wesata'
-};
-
-export default CONFIG;
