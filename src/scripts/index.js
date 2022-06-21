3b7d4a228368439fd531ea0d4039a4a77a16d48f
Rizky Fauzi Ardiansyah <90750376+Rizky0606@users.noreply.github.com>
2022-06-21 12:31:57 +0700

    Added list wisata slider for homepage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -30,12 +30,3 @@ window.addEventListener('hashchange', () => {
   });
 
 AOS.init();
-
-var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -65,6 +65,84 @@ const Home = {
             </div>
         </div>
 
+        <section class="destination"> 
+            <h2 class="destination-category">Top Destination</h2>
+            <button class="pre-btn"><i class="fa-solid fa-angle-left"></i></button>
+            <button class="nxt-btn"><i class="fa-solid fa-angle-right"></i></button>
+            <div class="destination-container">
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Bandung</span>
+                        <img src="https://s-light.tiket.photos/t/01E25EBZS3W0FY9GTG6C42E1SE/rsfit19201280gsm/events/2021/02/04/b9a441ee-a409-4897-b202-d20ddf1e558f-1612405052139-64d56f2449877ca5d518b0924113893f.jpg" class="destination-thumb" alt="Kawah Putih">
+                        <a href="/#/detail-wisata/7" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Kawah Putih</h2>
+                    </div>
+                </div>
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Kulon Progo</span>
+                        <img src="https://garasijogja.com/wp-content/uploads/2017/03/@imanuel_yuda.jpg" class="destination-thumb" alt="Air Terjun Kedung Pedut">
+                        <a href="/#/detail-wisata/9" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Air Terjun Kedung Pedut</h2>
+                    </div>
+                </div>
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Magelang</span>
+                        <img src="https://mmc.tirto.id/image/otf/700x0/2017/09/05/Borobudur--ISTOCK_ratio-16x9.jpg" class="destination-thumb" alt="Candi Borobudur">
+                        <a href="/#/detail-wisata/20" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Candi Borobudur</h2>
+                    </div>
+                </div>
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Raja Ampat</span>
+                        <img src="https://asset.kompas.com/crops/28S0TkvG7koDd1XpB5m9t-703Ww=/0x0:780x520/780x390/data/photo/2021/03/21/605753630590a.jpg" class="destination-thumb" alt="Raja Ampat">
+                        <a href="/#/detail-wisata/23" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Raja Ampat</h2>
+                    </div>
+                </div>
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Medan</span>
+                        <img src="https://awsimages.detik.net.id/community/media/visual/2020/09/13/danau-toba_169.jpeg?w=780&q=90" class="destination-thumb" alt="Danau Toba">
+                        <a href="/#/detail-wisata/26" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Danau Toba</h2>
+                    </div>
+                </div>
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Lombok Tengah</span>
+                        <img src="https://topjabar.co/wp-content/uploads/2022/01/The-mandalika-kuta-lombok-1024x640-1.jpg" class="destination-thumb" alt="Pantai Kuta Mandalika">
+                        <a href="/#/detail-wisata/46" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Pantai Kuta Mandalika</h2>
+                    </div>
+                </div>
+                <div class="destination-card">
+                    <div class="destination-image">
+                        <span class="destination-city">Bantul</span>
+                        <img src="https://suryayogya.com/wp-content/uploads/2020/06/parangtritis.jpg" class="destination-thumb" alt="Pantai Parangtritis">
+                        <a href="/#/detail-wisata/48" class="card-btn">Detail</a>
+                    </div>
+                    <div class="destination-info">
+                        <h2 class="destination-name">Pantai Parangtritis</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <div class="content-container-2" id="about-us" data-aos="zoom-in" data-aos-duration="2000">
             <div class="content-2">
                 <div class="content2_title">
@@ -93,6 +171,25 @@ const Home = {
         </div>
         `;
     }, 
+
+    afterRender() {
+        const destinationContainers = [...document.querySelectorAll('.destination-container')];
+        const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+        const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+        destinationContainers.forEach((item, i) => {
+            let containerDimensions = item.getBoundingClientRect();
+            let containerWidth = containerDimensions.width;
+        
+            nxtBtn[i].addEventListener('click', () => {
+                item.scrollLeft += containerWidth;
+            })
+        
+            preBtn[i].addEventListener('click', () => {
+                item.scrollLeft -= containerWidth;
+            })
+        })
+    }
 };
 
 export default Home;
